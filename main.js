240ce88e339c9dcb2b3235f4d24a88eca3732b68
tomikorzu <tomykorzu@icloud.com>
2024-07-29 18:47:04 -0300

    fix(navbar): change the navbar functionalities in main.js

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
-// Hamburguer button
-document.getElementById('hamburguer-button').addEventListener('click', function() {
-    this.classList.toggle('active');
-    document.getElementById('mobile-navbar-sections').classList.toggle('show');
-    document.querySelector('.navbar').classList.toggle('transparent');
+function toggleMenu() {
+    const navbar = document.querySelector('.navbar');
+    hamburgerButton.classList.toggle('active');
+    mobileNavSections.classList.toggle('show');
+    navbar.classList.toggle('transparent');
+}
+
+const hamburgerButton = document.getElementById('hamburguer-button');
+const mobileNavSections = document.getElementById('mobile-navbar-sections');
+const navLinks = document.querySelectorAll('.mobile-navbar-sections-a');
+
+hamburgerButton.addEventListener('click', toggleMenu);
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        if (hamburgerButton.classList.contains('active')) {
+            toggleMenu();
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const offset = 60; // Ajustar según la altura de la navbar
+    document.querySelectorAll('a[href^="#"]').forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href');
+            const targetPosition = document.querySelector(targetId).getBoundingClientRect().top + window.pageYOffset - offset;
+
+            window.scrollTo({
+                top: targetPosition,
+                behavior: 'smooth'
+            });
+        });
+    });
 });
