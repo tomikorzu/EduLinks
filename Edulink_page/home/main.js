631ccdcb67b74fb1428e253f73d385e4c18c78a5
tomikorzu <tomykorzu@icloud.com>
2024-08-21 18:54:25 -0300

    feat(create-channel / channel.css / main.js): add newChannelMenu style for all devices responsive

--- Edulink_page/home/main.js
+++ Edulink_page/home/main.js
@@ -1,7 +1,15 @@
-let chat = document.getElementById('group')
-let seccionChannel = document.getElementById('section-chanel')
-let seccionClases = document.getElementById('section-classes')
-chat.addEventListener('click', function(){
-        seccionChannel.style.display = 'none'
-        
+let newChannelButton = document.getElementById('new-channel')
+newChannelButton.addEventListener('click', function(){
+        addChannelInfo()
 })
+
+
+let body = document.querySelector('body')
+
+function addChannelInfo(){
+        console.log('funciona')
+        let newChannelMenu = document.createElement('form')
+        newChannelMenu.classList.add('new-channel-menu')
+        newChannelMenu.innerHTML = '<h2 class="h2-new-channel-menu">Nuevo canal</h2><figure class="figure-new-channel"><input type="file" class="input-image-receive" id="imageUpload"><label for="imageUpload" class="icon-label"><i class="fa-solid fa-plus create-channel-icon"></i></label><img src="${Image Channel}" alt="Image Channel" class="img-preview"></figure><button type="submit" class="change-img" id="change-img">Cambiar foto</button><div class="inputs-channel"><input type="text" placeholder="Nombre del canal" class="input-channel-name" id="input-channel-name"><input type="text" placeholder="Clave del canal" class="input-channel-key" id="input-channel-key"></div><button class="create-channel" id="create-channel" type="submit">Crear canal</button>'
+        body.append(newChannelMenu)
+}
