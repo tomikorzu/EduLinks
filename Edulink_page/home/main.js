1415fc1210b9164da53cd1d0e4f9173899e4d7c9
tomikorzu <tomykorzu@icloud.com>
2024-08-20 22:02:32 -0300

    feat(main.js): add an script to hide the channel section when we touch one group

---
+++ Edulink_page/home/main.js
@@ -0,0 +1,7 @@
+let chat = document.getElementById('group')
+let seccionChannel = document.getElementById('section-chanel')
+let seccionClases = document.getElementById('section-classes')
+chat.addEventListener('click', function(){
+        seccionChannel.style.display = 'none'
+        
+})
