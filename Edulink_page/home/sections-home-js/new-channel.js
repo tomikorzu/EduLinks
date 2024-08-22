7acdb163e52474a8a36f2ebb408a2f94dd44e539
tomikorzu <tomykorzu@icloud.com>
2024-08-22 18:45:21 -0300

    fix(Edulink_page): reorganize all files and folders and add dms file

--- Edulink_page/home/sections-home-js/new-channel.js
+++ Edulink_page/home/sections-home-js/new-channel.js
@@ -7,7 +7,7 @@ let header = document.querySelector('header')
 let aside = document.querySelector('aside')
 let sections = document.querySelectorAll('section')
 let groupsContainer = document.getElementById('chanel-container')
-let defaultFileImg = '../../img/defaultChannelImage.png'
+let defaultFileImg = '../../../img/defaultChannelImage.png'
 
 function addChannelInfo() {
     applyBlur(true)
